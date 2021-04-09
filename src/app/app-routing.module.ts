4d7a62e0e65c306aee173073746ede5633aa33ba
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2021-04-08 22:38:13 -0300

    chat module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'chat'
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./pages/chat/chat.module').then(m => m.ChatModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
