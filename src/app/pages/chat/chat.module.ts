d26a627274998f1d5ff84a60555203d3c1896426
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2021-04-22 23:23:03 -0300

    Input de pesquisa

--- src/app/pages/chat/chat.module.ts
+++ src/app/pages/chat/chat.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ChatComponent } from './chat.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SideComponent } from './side/side.component';
 import { WindowComponent } from './window/window.component';
+import { ListConversationsComponent } from './side/list-conversations/list-conversations.component';
+import { SearchInputComponent } from './search-input/search-input.component';
 
 const routes: Routes = [
   {
@@ -19,13 +23,17 @@ const routes: Routes = [
   declarations: [
     ChatComponent,
     SideComponent,
-    WindowComponent
+    WindowComponent,
+    ListConversationsComponent,
+    SearchInputComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class ChatModule { }
---
+++ src/app/pages/chat/side/list-conversations/list-conversations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-conversations',
+  templateUrl: './list-conversations.component.html',
+  styleUrls: ['./list-conversations.component.scss']
+})
+export class ListConversationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
